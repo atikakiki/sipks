e9723ad80687ea1bb19f00001ba54757dc43a614
unknown <atikarizki41@gmail.com>
2020-10-29 23:40:52 +0700

    add namespace RouteServiceProvider

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+// namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
