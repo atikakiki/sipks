12a2e72c09bc56144bb3b01b981c068c01e1efb8
unknown <atikarizki41@gmail.com>
2020-11-19 15:27:04 +0700

    input detail

--- resources/views/directory/buatDetailPengajuan.blade.php
+++ resources/views/directory/buatDetailPengajuan.blade.php
@@ -19,74 +19,113 @@
 </section>
 
 <section class="content"> 
-<div class="row">
+    <div class="row">
 	  <div class="col-xs-12">
 	    <div class="panel panel-warning">  
-        <div class="box-body">
-        <form action="{{ url('pengajuan/postDetail')}}" method="POST">
-        @csrf
-                <div class = "field_wrapper">
-                <!-- <input type="text" class="form-control" name="nama_detail[]" value="'.$pengajuan->id_pengajuan'"> -->
-                <!-- <button type="button" onclick="return add()" class="btn btn-primary" id="add_button" >Tambah</button> -->
-                <div class="form-group">
-                  <label>Nama Detail</label>
-                  <input type="text" class="form-control" name="nama_detail" placeholder="Masukkan Nama Detail" required>
+            <div class="box-body">
+                <div class="table-responsive">
+                    <form method="post" id="detail_form">
+                    <span id="result"></span>
+                        <table class="table table-bordered table-striped" id="user_table">
+                        <thead>
+                        <tr>
+                            <th width="35%">Nama Detail</th>
+                            <th width="10%">Jumlah</th>
+                            <th width="25%">Harga Satuan</th>
+                            <th width="30%">Total</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                            <td colspan="4" align="right">&nbsp;</td>
+                            <td>
+                            @csrf
+                                <input type="submit" name="save" id="save" class="btn btn-primary" value="Save" />
+                            </td>
+                            </tr>
+                        </tfoot>
+                        </table>
+                     </form>
                 </div>
-                <div class="form-group">
-                  <label>Jumlah</label>
-                  <input type="text" class="form-control" name="jumlah_detail" placeholder="Masukkan Jumlah" required>
-                </div>
-                <div class="form-group">
-                  <label>Harga Satuan</label>
-                  <input type="text" class="form-control" name="harga_satuan_detail" placeholder="Masukkan Harga Satuan" required>
-                </div>
-                <div class="form-group">
-                  <label>Total</label>
-                  <input type="text" class="form-control" name="total_harga_detail" placeholder="Masukkan Total Harga" required>
-                </div>
-              </div> 
-              
-                </div>
-                <div class="box-footer">
-                <button type="button" onclick="location.href='{{ url('/pengajuan')}}'" class="btn btn-default pull-left" >Close</button>
-                <button type="submit" class="btn btn-success pull-right">Submit</button>
-              </div>
-            </form>
-	    </div>
-	  </div>
-	</div>
+            </div>
+         </div>
+     </div>
+   </div>
   </section>
 @endsection
 
 @section('moreJS')
-  <script type="text/javascript">
-    // function add(){
-    //     // var maxField = 10; //Input fields increment limitation
-    //     //var addButton = $('#add_button'); //Add button selector
-    //     var wrapper = $('.field_wrapper'); //Input field wrapper
-    //     var fieldHTML = '<div class="form-group"><label>Nama Detail</label><input type="text" class="form-control" name="nama_detail[]" placeholder="Masukkan Nama Detail" required></div>';
-    //     fieldHTML=fieldHTML + '<div class="form-group"><label>Jumlah</label><input type="text" class="form-control" name="jumlah_detail[]" placeholder="Masukkan Jumlah" required></div>';
-    //     fieldHTML=fieldHTML + '<div class="form-group"><label>Harga Satuan</label><input type="text" class="form-control" name="harga_satuan_detail[]" placeholder="Masukkan Harga Satuan" required></div>'; 
-    //     fieldHTML=fieldHTML + '<div class="form-group"> <label>Total</label><input type="text" class="form-control" name="total_harga_detail[]" placeholder="Masukkan Total Harga" required></div>'; 
-    //     fieldHTML=fieldHTML + '<button type="button" class="btn btn-danger" >Hapus</button>'; 
-    //     var x = 1; //Initial field counter is 1
-     
-    // // //Once add button is clicked
-    // //     $(addButton).click(function(){
-    // //         //Check maximum number of input fields
-    //         // if(x < maxField){ 
-    //         //     x++; //Increment field counter
-    //             $(wrapper).append(fieldHTML); //Add field html
-    //         // }
-    //     }
-     
-    //Once remove button is clicked
-//         $(wrapper).on('click', '.remove_button', function(e){
-//             e.preventDefault();
-//             $(this).parent('').parent('').remove(); //Remove field html
-//             x--; //Decrement field counter
-//         });
-// }
+<script type="text/javascript">
+$(document).ready(function(){
+    var count = 1;
+    detail_field(count);
 
-  </script>
+    function detail_field(number)
+    {
+        html = '<tr>';
+        html += '<td><input type="text" name="nama_detail[]" class="form-control" /></td>';
+        html += '<td><input type="text" name="jumlah_detail[]" class="form-control" /></td>';
+        html += '<td><input type="text" name="harga_satuan_detail[]" class="form-control" /></td>';
+        html += '<td><input type="text" name="total_harga_detail[]" class="form-control" /></td>';
+        if(number > 1)
+        {
+            html += '<td><button type="button" name="remove" id="" class="btn btn-danger remove">Hapus</button></td></tr>';
+            $('tbody').append(html);
+        }
+        else
+        {   
+            html += '<td><button type="button" name="add" id="add" class="btn btn-success">Tambah</button></td></tr>';
+            $('tbody').html(html);
+        }
+    }
+
+$(document).on('click', '#add', function(){
+ count++;
+ detail_field(count);
+});
+
+$(document).on('click', '.remove', function(){
+ count--;
+ $(this).closest("tr").remove();
+});
+
+$('#detail_form').on('submit', function(event){
+       event.preventDefault();
+       $.ajax({
+           url:'{{ route("detail-field.insert") }}',
+           method:'post',
+           data:$(this).serialize(),
+           dataType:'json',
+           beforeSend:function(){
+               $('#save').attr('disabled','disabled');
+           },
+           success: function(response) {
+            window.location.replace('{{ url('/pengajuan') }}');
+             }
+        //    success:function(data)
+        //    {
+        //        if(data.error)
+        //        {
+        //            var error_html = '';
+        //            for(var count = 0; count < data.error.length; count++)
+        //            {
+        //                error_html += '<p>'+data.error[count]+'</p>';
+        //            }
+        //            $('#result').html('<div class="alert alert-danger">'+error_html+'</div>');
+        //        }
+        //        else
+        //        {
+        //            detail_field(1);
+        //            $('#result').html('<div class="alert alert-success">'+data.success+'</div>');
+        //        }
+        //        $('#save').attr('disabled', false);
+        //    }
+       })
+});
+
+});
+</script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,16 +45,16 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
+                        <!-- @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ url('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ url('register') }}">{{ __('Register') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ url('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ url('/') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-                        @else
+                        @else -->
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -72,7 +72,7 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                        <!-- @endguest -->
                     </ul>
                 </div>
             </div>
